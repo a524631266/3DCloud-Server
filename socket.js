ee6be86b4b18352884212fd844c40cde53ec07bb
Nicolas Gnyra <nicolas@gnyra.com>
2017-11-21 12:45:37 -0500

    Add name to printer profile

--- socket.js
+++ socket.js
@@ -1,6 +1,9 @@
+let util = require('util');
+
 let profiles = [
     {
         "id": "d3b8b322d33baad341613523f1fc5fe327ca400eb8baef2f10216a48bc616603",
+        "name": "RPL2-04",
         "host_id": 190016740214819,
         "type": "makerbot"
     }
@@ -23,11 +26,12 @@ module.exports = (server) => {
         });
 
         client.on('printer', function (data, ack) {
-            console.log('got printer: ' + JSON.stringify(data));
+            console.log('Received request for printer with ID ' + data['device']['id']);
 
             for (let i = 0; i < profiles.length; i++) {
                 if (profiles[i]['host_id']   === data['host_id'] &&
                     profiles[i]['id'] === data['device']['id']) {
+                    console.log(util.format('Sending printer profile for %s (%s)', profiles[i]['id'], profiles[i]['name']));
                     ack({'defined': true, 'profile': profiles[i]});
                 }
             }
