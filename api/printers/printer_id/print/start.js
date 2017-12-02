18c33e2b2862af541e186bbeb35c1fec475a7247
nicoco007 <CQ]9CTm!!FqC.nKG>
2017-12-01 20:35:40 -0500

    Fix crash when trying to print on disconnected host

--- api/printers/printer_id/print/start.js
+++ api/printers/printer_id/print/start.js
@@ -9,19 +9,23 @@ module.exports = function(db, io) {
             let machineId = io.devices[printerId];
             let fileId = req.query['id'];
 
-            if (!io.hosts[machineId])
-                res.error('Host is not connected.');
-
             let file = await db.getFile(fileId);
 
             if (!file) {
                 res.error('File not found');
                 return;
             }
-
-            io.hosts[machineId].emit('print', {'printer_id': printerId, 'key': file.key, 'name': file.name}, function(data) {
-                res.json(data);
-            });
+            if (io.hosts[machineId]) {
+                io.hosts[machineId].emit('print', {
+                    'printer_id': printerId,
+                    'key': file.key,
+                    'name': file.name
+                }, function (data) {
+                    res.json(data);
+                });
+            } else {
+                res.error('Host is not connected.');
+            }
         }
     }
 };
